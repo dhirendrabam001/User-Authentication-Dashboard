7f4cc1520556aa46028be285c3382adc4bb62a18
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-08-23 10:39:08 -0700

    updated information

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,33 @@
-const registerData = (req,res) => {
+const bcrypt = require("bcrypt");
+const userModel = require("../models/user");
+const registerData = async (req,res) => {
     const {username, email, password, cpassword} = req.body;
-    console.log(req.body);
     
+    // Check all field are required or not
+    if(!username || !email || !password || !cpassword) {
+        return res.status(400).json({success: false, message: "Please field the neccessary requirements"});
+    };
 
-}
+    // Compare password and cpassword
+    if(password !== cpassword) {
+        return res.status(401).json({success: false, message: "Password does not matched"});
+    };
+
+    // Password Hashing
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log(hashPassword);
+    
+    // Create and save database data
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashPassword,
+    });
+
+    await user.save();
+    res.status(200).json({success: true, message: "User Register Successfully"});
+
+};
 
 
 
